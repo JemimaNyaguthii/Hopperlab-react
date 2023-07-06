68a75e9c174c2891186b640c337086da45cb3720
JemimaNyaguthii <jemimanyaguthiiwambugu@gmail.com>
2023-07-06 12:15:56 +0300

    Updates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Login from './LogIn';
+import Products from './Products';
+
+function App() {
+  return (
+    <div>
+     <Login/>
+     <Products/>
+    </div>
+    
+  );
+}
+
+export default App;
